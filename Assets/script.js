79a08e3e01df74c5430aacac9e85f20e5bf2184b
Mikell <Mikellt@live.com>
2022-07-07 02:39:41 -0700

    minor updates to js

--- Assets/script.js
+++ Assets/script.js
@@ -1,47 +1,54 @@
 // console.log("Hello from file")
 
+// Declare the Current time
 var today = moment();
 $("#currentDay").text(today.format("dddd, MMMM Do"));
 
 // Declare Current hour
 
-// Declare class=container and id="timeEntries"
+// Declare Current Day HTML Element
 
 //Declare Time Entries Container HTML element
 var containerE1;
 
 // Render a block for each hour of the day
+// hint: "hour-*hour number*" using for loop
+for (var i = 9; i <= 17; i++){
 
-    // hint: "hour-*hour number*" using for loop
-    for (var i = 9; i <= 17; i++){
+    // Attempt to get the saved data for the hour of the loop
+    var key = "hour-"+i;
 
-        // Attempt to get the saved data for the hour of the loop
-        var key = "hour-"+i;
+    var data = "";
 
-        var data = "asdf";
+    // Compare i to current hour to determine if this hour is in the past, present, or future
 
-        // Compare i to current hour to determine if this hour is in the past, present, or future
+    var template = `
+        <div class="row">
+            <div>
+                ${i}am
+            </div>
+            <div>
+                <textarea></textarea>
+            </div>
+            <div>
+                <button data-hour="${i}">Save</button>
+            </div>
+        </div>"
+    `;
 
-        var template = `
-            <div class="row">
-                <div>
-                    ${i}am
-                </div>
-                <div>
-                    <textarea></textarea>
-                </div>
-                <div>
-                    <button data-hour="${i}">Save</button>
-                </div>
-            </div>"
-        `;
-
-        // Append the html to the page using container element (line 7)
-    }
+    // Append the html to the page using container element (line 7)
+}
 
 // Set up a "click" event listener on the container
 containerE1.on("click", "button", function(event){
 
-})
+//Fetch the hour from the clicked button's "data-hour" attribute
 
-// Save an hour to local storage
+// Use the hour to create the key for local storage
+event.target.matches(key);
+
+//From the clicked button, traverse the DOM to the nearby <textarea> to fetch its value
+
+// Use the key and the value to save into localStorage
+
+})

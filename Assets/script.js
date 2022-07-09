a7c1500049cad31cf264b425e96c522067666118
Mikell <Mikellt@live.com>
2022-07-09 01:16:23 -0700

    js, html, readme completed

--- Assets/script.js
+++ Assets/script.js
@@ -1,140 +1,55 @@
-// console.log("Hello from file")
+$(document).ready(function () {
 
-// Declare the Current time
-var today = moment();
-$("#currentDay").text(today.format("dddd, MMMM Do"));
+    //declare current day & time.
+    $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
 
-// Declare Current hour
+    //declare button function and asigne even listener
+    $(".saveBtn").on("click", function () {
+        console.log(this);
+        var text = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
 
-// Declare Current Day HTML Element
+        //establish local storage
+        localStorage.setItem(time, text);
+    })
 
-//Declare Time Entries Container HTML element
-var containerE1;
+    $("#hour9 .description").val(localStorage.getItem("hour9"));
+    $("#hour10 .description").val(localStorage.getItem("hour10"));
+    $("#hour11 .description").val(localStorage.getItem("hour11"));
+    $("#hour12 .description").val(localStorage.getItem("hour12"));
+    $("#hour13 .description").val(localStorage.getItem("hour13"));
+    $("#hour14 .description").val(localStorage.getItem("hour14"));
+    $("#hour15 .description").val(localStorage.getItem("hour15"));
+    $("#hour16 .description").val(localStorage.getItem("hour16"));
+    $("#hour17 .description").val(localStorage.getItem("hour17"));
 
-// Render a block for each hour of the day
-// hint: "hour-*hour number*" using for loop
+    //declare hour tracker
+    function hourTracker() {
+        
+        var currentHour = moment().hour();
 
-//     // Attempt to get the saved data for the hour of the loop
-//     var key = "hour-"+i;
+        // loop time blocks
+        $(".time-block").each(function () {
+            var blockHour = parseInt($(this).attr("id").split("hour")[1]);
+            console.log( blockHour, currentHour)
 
-//     var data = "";
-//     // Append the html to the page using container element (line 7)
-// }
-
-// Set up a "click" event listener on the container
-// containerE1.on("click", "button", function(event){
-
-// //Fetch the hour from the clicked button's "data-hour" attribute
-
-// // Use the hour to create the key for local storage
-// event.target.matches(key);
-
-//From the clicked button, traverse the DOM to the nearby <textarea> to fetch its value
-
-// Use the key and the value to save into localStorage
-
-
-document.querySelector("#button-9").onclick =function(){
-    var text =document.querySelector("#hour-9").value;
-    localStorage.setItem("hour9", JSON.stringify(text));
-    document.querySelector("#hour-9").textContent = JSON.parse(localStorage.getItem("hour9"))
-
-    if(localStorage.getItem("hour9")) 
-    {
-    document.querySelector("#hour-9").textContent = JSON.parse(localStorage.getItem("hour9"))}
-}
-
-document.querySelector("#button-10").onclick =function(){
-    var text =document.querySelector("#hour-10").value;
-    localStorage.setItem("hour10", JSON.stringify(text));
-    document.querySelector("#hour-10").textContent = JSON.parse(localStorage.getItem("hour10"))
-   
-   if(localStorage.getItem("hour10")) 
-   {
-   document.querySelector("#hour-10").textContent = JSON.parse(localStorage.getItem("hour10"))}
-   }
-
-document.querySelector("#button-11").onclick =function(){
-    var text =document.querySelector("#hour-11").value;
-    localStorage.setItem("hour11", JSON.stringify(text));
-    document.querySelector("#hour-11").textContent = JSON.parse(localStorage.getItem("hour11"))
-   
-   if(localStorage.getItem("hour11")) 
-   {
-   document.querySelector("#hour-11").textContent = JSON.parse(localStorage.getItem("hour11"))}
-   }
-
-document.querySelector("#button-12").onclick =function(){
-    var text =document.querySelector("#hour-12").value;
-    localStorage.setItem("hour12", JSON.stringify(text));
-    document.querySelector("#hour-12").textContent = JSON.parse(localStorage.getItem("hour12"))
-
-   if(localStorage.getItem("hour12")) 
-   {
-   document.querySelector("#hour-12").textContent = JSON.parse(localStorage.getItem("hour12"))}
-   }
-
-document.querySelector("#button-13").onclick =function(){
-    var text =document.querySelector("#hour-13").value;
-    localStorage.setItem("hour13", JSON.stringify(text));
-    document.querySelector("#hour-13").textContent = JSON.parse(localStorage.getItem("hour13"))
-   
-   if(localStorage.getItem("hour13")) 
-   {
-   document.querySelector("#hour-13").textContent = JSON.parse(localStorage.getItem("hour13"))}
-   }
-
-document.querySelector("#button-14").onclick =function(){
-    var text =document.querySelector("#hour-14").value;
-    localStorage.setItem("hour14", JSON.stringify(text));
-    document.querySelector("#hour-14").textContent = JSON.parse(localStorage.getItem("hour14"))
-   
-   if(localStorage.getItem("hour14")) 
-   {
-   document.querySelector("#hour-14").textContent = JSON.parse(localStorage.getItem("hour14"))}
-   }
-
-document.querySelector("#button-15").onclick =function(){
-    var text =document.querySelector("#hour-15").value;
-    localStorage.setItem("hour15", JSON.stringify(text));
-    document.querySelector("#hour-15").textContent = JSON.parse(localStorage.getItem("hour15"))
-
-   if(localStorage.getItem("hour15")) 
-   {
-   document.querySelector("#hour-15").textContent = JSON.parse(localStorage.getItem("hour15"))}
-   }
-
-document.querySelector("#button-16").onclick =function(){
-    var text =document.querySelector("#hour-16").value;
-    localStorage.setItem("hour16", JSON.stringify(text));
-    document.querySelector("#hour-16").textContent = JSON.parse(localStorage.getItem("hour16"))
-
-   if(localStorage.getItem("hour16")) 
-   {
-   document.querySelector("#hour-16").textContent = JSON.parse(localStorage.getItem("hour16"))}
-   }
-
-document.querySelector("#button-17").onclick =function(){
-    var text =document.querySelector("#hour-10").value;
-    localStorage.setItem("hour17", JSON.stringify(text));
-    document.querySelector("#hour-17").textContent = JSON.parse(localStorage.getItem("hour17"))
-
-   if(localStorage.getItem("hour17")) 
-   {
-   document.querySelector("#hour-17").textContent = JSON.parse(localStorage.getItem("hour17"))}
-   }
-
-// for (var i=9; i++; i<11){
-
-//     document.querySelectorAll(".saveBtn").onclick =function(){
-//         for (var i=9; i++; i<11){
-//         var text =document.querySelector(`#hour-${i}`).value;
-//         localStorage.setItem(`hour${i}`, JSON.stringify(text));
-//         document.querySelector(`#hour-${i}`).textContent = JSON.parse(localStorage.getItem(`hour${i}`))
-//         }
-    
-//     if(localStorage.getItem(hour)) 
-//     {
-//     document.querySelector(`#hour-${i}`).textContent = JSON.parse(localStorage.getItem(`hour${i}`))}}
-    
-//     }
+            //check if hour is past, present, or future time
+            if (blockHour < currentHour) {
+                $(this).addClass("past");
+                $(this).removeClass("future");
+                $(this).removeClass("present");
+            }
+            else if (blockHour === currentHour) {
+                $(this).removeClass("past");
+                $(this).addClass("present");
+                $(this).removeClass("future");
+            }
+            else {
+                $(this).removeClass("present");
+                $(this).removeClass("past");
+                $(this).addClass("future");
+            }
+        })
+    }
+    hourTracker(); 
+})
